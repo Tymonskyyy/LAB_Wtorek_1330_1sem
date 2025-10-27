8e891fd62c60da7923b2bd185b69b2049441f120
Tymonskyyy <lawecki.szymon24@gmail.com>
2025-10-27 18:15:51 +0100

    Zadanie 8 dokończ

---
+++ LAB03/Zadanie 8/Zadanie 8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int user_number;
+	cout << "Podaj liczbê: \n";
+	cin >> user_number;
+	if (user_number < 0) {
+		cout << "Liczba jest ujemna\n";
+	}
+	else if (user_number > 0) {
+		if (user_number < 2) {
+
+		}
+	}
+}
+	
