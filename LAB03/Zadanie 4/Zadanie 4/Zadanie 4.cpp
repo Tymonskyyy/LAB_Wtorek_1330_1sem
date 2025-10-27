5537d9cbbc0ae9c066eed08078ae90ae7afd88e6
Tymonskyyy <lawecki.szymon24@gmail.com>
2025-10-27 17:53:22 +0100

    Kalkulator end

---
+++ LAB03/Zadanie 4/Zadanie 4/Zadanie 4.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	int b;
+	char decyzja;
+	float wynik;
+	cout << "Podaj liczbê pierwsz¹: \n";
+	cin >> a;
+	cout << "Podaj liczbê drug¹: \n";
+	cin >> b;
+	cout << "Co chcesz zrobiæ: (+-dodawanie. - - odejmowanie itd \n";
+	cin >> decyzja;
+	if (decyzja == '+') {
+		wynik = a + b;
+		cout << "wynik to: ";
+		cout << wynik;
+	}
+	if (decyzja == '-') {
+		wynik = a - b;
+		cout << "wynik to: ";
+		cout << wynik;
+
+	}
+	if (decyzja == '*') {
+		wynik = a * b;
+		cout << "wynik to: ";
+		cout << wynik;
+	}
+	if (decyzja == '/') {
+		if (b == 0) {
+			cout << "Nie dzielimy przez 0!";
+		}
+		else {
+			wynik = (float)a / b;
+			cout << "wynik to: ";
+			cout << wynik;
+		}
+	}
+}
