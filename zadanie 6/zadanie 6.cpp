4b1a498c8dc51fc6c0d77b23d55b953565395f27
Tymonskyyy <lawecki.szymon24@gmail.com>
2025-10-27 16:53:26 +0100

    zadanie 7

--- zadanie 6/zadanie 6.cpp
+++ zadanie 6/zadanie 6.cpp
@@ -6,8 +6,12 @@ int main() {
 	for (int i = 0; i < 10; i++) {
 		cout << "Podaj cyfre";
 		cin >> cyfra_user;
+		if (cyfra_user < 0) {
+			cout << "Musi byæ dodatnia" << endl;
+			return 7;
+		}
+		else{
+			continue;
+		}
 	}
-}
-	return 0;
-
 }
