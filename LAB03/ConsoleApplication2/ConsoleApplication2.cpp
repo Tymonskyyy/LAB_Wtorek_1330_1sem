5a1c8bbed05db4abfd20c87f5829490c35fa744f
Tymonskyyy <lawecki.szymon24@gmail.com>
2025-10-27 18:00:57 +0100

    Zadanie 6

---
+++ LAB03/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int prime = true;
+	while (prime == true) {
+		int a;
+		int b;
+		char decyzja;
+		float wynik;
+		char koniec;
+		cout << "\nczy chcesz zakończyć (t/n)";
+		cin >> koniec;
+		if (koniec == 't'){
+			prime = false;
+				break;
+		}			
+		cout << "Podaj liczbê pierwsz¹: \n";
+		cin >> a;
+		cout << "Podaj liczbê drug¹: \n";
+		cin >> b;
+		cout << "Co chcesz zrobiæ: (+-dodawanie. - - odejmowanie itd \n";
+		cin >> decyzja;
+		if (decyzja == '+') {
+			wynik = a + b;
+			cout << "wynik to:\n ";
+			cout << wynik, "\n";
+
+		}
+		if (decyzja == '-') {
+			wynik = a - b;
+			cout << "wynik to:\n";
+			cout << wynik, "\n";
+
+		}
+		if (decyzja == '*') {
+			wynik = a * b;
+			cout << "wynik to: \n";
+			cout << wynik,"\n";
+		}
+		if (decyzja == '/') {
+			if (b == 0) {
+				cout << "Nie dzielimy przez 0!\n";
+			}
+			else {
+				wynik = (float)a / b;
+				cout << "wynik to:\n ";
+				cout << wynik,"\n";
+			}
+		}
+	}
+}
