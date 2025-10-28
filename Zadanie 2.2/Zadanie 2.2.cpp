fd79e30177d0f9fea0e045950f93b8b9f63f39b3
Krzyhu <lawecki.szymon24@gmail.com>
2025-10-28 14:35:12 +0100

    v1.0.9

---
+++ Zadanie 2.2/Zadanie 2.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iosstream>
+using namespace std;
+int fun1(int a)
+{
+	cin >> a;
+	return a;
+
+}
+int main()
+{
+	int a;
+	cout << "Podaj liczbe: \n";
+	fun1(a);
+	cout << "Podales liczbe: " << a << endl;
+	return 0;
+}
+
