e197bd7b8ab7553a916d943d095be1d7314c4d9e
Krzyhu <lawecki.szymon24@gmail.com>
2025-10-28 14:21:15 +0100

    Lab 04 zad 2 v 1.6

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int fun1(int a)
+{
+	cout << "Podaj pierwszą liczbę: \n";
+	cin >> a;
+}
+int main() {
+	cout << "Podaj pierwszą liczbę: \n";
+
+}
---
+++ LAB04/Zadanie 2/Zadanie 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int fun1(int a)
+{
+	cout << "Podaj pierwsz¹ liczbê: \n";
+	cin >> a;
+}
+int main() {
+	cout << "Podaj pierwsz¹ liczbê: \n";
+
+}
