d5022423b26d14ff7a9da73f795f719c185e2ecd
Krzyhu <lawecki.szymon24@gmail.com>
2025-11-06 15:13:28 +0100

    Zadanie 4 END

---
+++ LAB05/Zadanie 4.1/Zadanie 4.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int wiersze = 0; //
+	int kolumny = 0;
+	int i = 0;
+	int x = 0;
+	cout << "Podaj ilość wierszy: " << endl;
+	cin >> wiersze;
+	cout << "Podaj ilość kolumn: " << endl;
+	cin >> kolumny;
+	int** tablice = new int* [wiersze];
+	for (i = 0; i < wiersze; i++) {
+		tablice[i] = new int[kolumny];
+	}
+	cout << "Podaj elementy tablicy: " << endl;
+	for (i = 0; i < wiersze; i++) {
+		for (x = 0; x < kolumny; x++) {
+			cout << "Element [" << i << "][" << x << "]: ";
+			cin >> tablice[i][x];
+		}
+	}
+	cout << "Tablica: " << endl;
+	for (i = 0; i < wiersze; i++) {
+		for (x = 0; x < kolumny; x++) {
+			cout << tablice[i][x] << " ";
+		}
+		cout << endl;
+	}
+}
