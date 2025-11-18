e95b3cd098b7b46022e739540d71218ece85e330
Krzyhu <lawecki.szymon24@gmail.com>
2025-11-18 14:12:59 +0100

    LAB06

---
+++ LAB05/Zadanie 5/Zadanie 5.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int tablica[10];
+	int i = 0;
+	
+	int prime = 1;
+	while (prime == 1) {
+		cout << "Wybierz opcje: " << endl;
+		cout << "1. Wprowadz liczby do tablicy" << endl;
+		cout << "2. Wyswietl liczby z tablicy" << endl;
+		cout << "3. Zakończ" << endl;
+		int wybor;
+		int i = 0;
+		int x = 0;
+		int liczba = 0;
+		cin >> wybor;
+		switch (wybor) {
+		case 1:
+			cout << "Wprowadzanie liczby";
+			cout << "----------------" << endl;
+			cout << "Podaj liczbę: \n" << endl;
+			cin >> liczba;
+
+			break;
+		case 2:
+			for (int i = 0; i < 10; i++) {
+				cout << tablica[i] << " ";
+			}
+			break;
+
+		case 3:
+			if (i < 10) {
+				for (i; i > 10; i++) {
+					tablica[i] = 0;
+				}
+			}
+			cout << "Twoja tablica :" << endl;
+			for (i = 0; i < 10; i++) {
+				cout << tablica[i] << " ";
+			}
+			prime = 0;
+		}
+	}
+}
+
+	
+	
---
+++ LAB06/Zadanie 1/Zadanie 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int globalna_a = 20;
+int globalna_b = 10;
+
+static int suma() {
+	int suma = globalna_a + globalna_b;
+
+	return globalna_a;
+	return globalna_b;
+
+}
+int main() {
+	suma();
+	int a = 1;
+	int b = 2;
+	suma(a, b);
+}
