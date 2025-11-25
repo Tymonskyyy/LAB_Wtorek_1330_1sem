63503241e353d974cc614fccf628479778b46ba1
Krzyhu <lawecki.szymon24@gmail.com>
2025-11-25 13:42:58 +0100

    LAB07

--- LAB06/Zadanie 3/Zadanie 3.cpp
+++ LAB06/Zadanie 3/Zadanie 3.cpp
@@ -34,36 +34,12 @@ static float wyswietl(double zapisaana) {
 
 }
 int main() {
-		int decyzja = 0;
-		double zapisana = 0;
-		cout << "Program do przeliczania temperatur Celsjusza na Kelwiny." << endl;
-		cout << "Mozesz zapisac maksymalnie 10 temperatur." << endl;
-		cout << "-----------------------------------------------" << endl;
-		cout << "Menu: " << endl;
-		cout << "1. Przelicz temperature Celsjusza na Kelwiny." << endl;
-		cout << "2. Zapisz temperature." << endl;
-		cout << "3. Wyswietl zapisane temperatury." << endl;
-		cout << "Podaj decyzje: " << endl;
-		cin >> decyzja;
-		if (decyzja < 1 || decyzja > 3) {
-			cout << "Nieprawidlowa decyzja. Program zakonczony." << endl;
-			return 0;
-		}
-		if (index >= 10) {
-			cout << "Osiagnieto maksymalna liczbe zapisanych temperatur. Program zakonczony." << endl;
-			return 0;
-		}
-		if (decyzja == 1) {
-			cout << "Podaj temperature do przeliczenia: " << endl;
-			cin >> zapisana;
-			przelicz(zapisana);
-		}
-		if (decyzja == 2) {
-			zapisz(zapisana);
-		}
-		if (decyzja == 3) {
-			wyswietl(zapisana);
-		}
-		return 0;
-	
+	double zapisana = 0;
+	cout << "Program do przeliczania temperatur Celsjusza na Kelwiny i zapisywania ich." << endl;
+	cout << "Podaj temperature w Celsjuszach: ";
+	cin >> zapisana;
+	przelicz(zapisana);
+	zapisz(zapisana);
+	wyswietl(zapisana);
+	return 0;
 }
