e2e6805399df9f62035b3e11f7fa0754fbbd9bb9
Krzyhu <lawecki.szymon24@gmail.com>
2025-12-02 21:25:08 +0100

    V1.5

---
+++ LAB07/ZAD1.1/Zadanie 5/Zadanie 5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int najwiekszyElement(int* tablica, int wielkosc) {
+	int zmienna_pomocnicza = 0;
+	for (int i = 0; i < wielkosc; i++) {
+		if (tablica[i] > zmienna_pomocnicza) {
+			zmienna_pomocnicza = tablica[i];
+		}
+	}
+	return zmienna_pomocnicza;
+}
+
+int main() {
+	int wielkosc;
+	cout << "Podaj wielkosc tablicy: ";
+	cin >> wielkosc;
+	int* tablica = new int[wielkosc];
+	for (int i = 0; i < wielkosc; i++) {
+		cout << "Podaj element " << i + 1 << ": ";
+		cin >> tablica[i];
+	}
+	najwiekszyElement(tablica, wielkosc);
+	cout << "Najwiekszy element tablicy to: " << najwiekszyElement(tablica, wielkosc) << endl;
+	return  0;
+}
