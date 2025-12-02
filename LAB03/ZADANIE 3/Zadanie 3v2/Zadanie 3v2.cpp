83bb85cfc8daf750371db6eca0a66977279966d3
Krzyhu <lawecki.szymon24@gmail.com>
2025-12-02 19:07:35 +0100

    LAB 03 END

--- LAB03/ZADANIE 3/Zadanie 3v2/Zadanie 3v2.cpp
+++ LAB03/ZADANIE 3/Zadanie 3v2/Zadanie 3v2.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+	cout << "Program do sprawdzania wspolczynnika r w odpowiedzi" << endl;
 	float r;
-	cout <<  "Podaj wpó³czynnik r: \n";
+	cout << "Podaj wartosc wspolczynnika r: ";
 	cin >> r;
-	if (r <= 0.7 && r <= 1) {
-		cout << "Bardzo silna korelacja dodatnia";
-	}
-		if (r <= 0.5) {
-		cout << "Silna korelacja dodatnia";
+	int prime = true;
+	while (prime == true){
+		if (r < 0.7 && r >= 1) {
+			cout << "Bardzo silna korelacja dodatnia" << endl;
+			prime = false;
 		}
-		if (r >= 0.3) {
-		cout << "umiarkowana korelacja dodatnia";
-	}
-	if (r >= 0.2) {
-		cout << "s³aba korelacja dodatnia ";
-	}
-	if (r >= 0) {
-		cout << " Brak korelacji";
+		if (r >= 0.5) {
+			cout << "Silna korelacja dodatnia" << endl;
+			prime = false;
+		}
+		if (r < 0.3) {
+			cout << "Umiarkowana korelacja dodatnie" << endl;
+			prime = false;
+		}
+		if (r < 0.2) {
+			cout << "S³aba korelacja dodatnia" << endl;
+			prime = false;
+		}
+		if (r = 0) {
+			cout << "Brak korelacji" << endl;
+			prime = false;
+		}
 
 	}
 }
