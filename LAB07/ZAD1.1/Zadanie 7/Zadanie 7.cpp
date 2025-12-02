7e38ec3c868865f0f307072b577e784c88428e70
Krzyhu <lawecki.szymon24@gmail.com>
2025-12-02 22:22:28 +0100

    V1.7

---
+++ LAB07/ZAD1.1/Zadanie 7/Zadanie 7.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int potegi( int liczba, int liczba_potegi) {
+	int potega = liczba_potegi;
+	for (int i = 1; i < liczba_potegi; i++) {
+		potega = potega * liczba;
+	}
+	return potega;
+
+}
+int main() {
+	int liczba = 0 ;
+	int liczba_potegi = 0;
+	cout << " Program do liczenia poteg" << endl;
+	cout << " Podaj liczbe calkowita: ";
+	cin >> liczba;
+	cout << "Podaj liczbe do ktorej chcesz obliczyc potegi: ";
+	cin >> liczba_potegi;
+	potegi(liczba, liczba_potegi);
+	cout << " Potega liczby " << liczba << " wynosi: " << potegi(liczba,liczba_potegi) << endl;
+}
