fffb50950ff1cb147c906e7401a69820c0c7bdc0
Krzyhu <lawecki.szymon24@gmail.com>
2025-12-02 22:10:51 +0100

    V1.6

---
+++ LAB07/ZAD1.1/Zadanie 6/Zadanie 6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int ciag(int n) {
+    int a = 0; 
+    int b = 1; 
+    int wynik = b; 
+
+
+    for (int i = 2; i <= n; i++) {
+        wynik = a + b;
+        a = b;   
+        b = wynik;
+    }
+    return wynik;
+
+}
+int main() {
+	int n;
+	int prime = 1;
+	cout << " Podaj liczbe calkowita n: ";
+	cin >> n;
+	ciag(n);
+	cout << " Wartosc ciagu dla n = " << n << " wynosi: " << ciag(n) << endl;
+
+}
