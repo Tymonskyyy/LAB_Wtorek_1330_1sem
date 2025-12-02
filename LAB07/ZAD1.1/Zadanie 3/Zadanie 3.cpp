b4609323ca4b7f4f4cd53176a0878624e4611f7e
Krzyhu <lawecki.szymon24@gmail.com>
2025-12-02 14:28:32 +0100

    V1.3

---
+++ LAB07/ZAD1.1/Zadanie 3/Zadanie 3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+string odwroc(string wyraz) {
+	int n = wyraz.length();
+	string odwrocony_wyraz;
+	for (int i = 0; i < n / 2; i++) {
+		swap(wyraz[i], wyraz[n - i - 1]);
+	}
+	return wyraz;
+}
+int main() {
+	cout << "Odrwacam liczbe" << endl;
+	string wyraz;
+	cout << "Podaj wyraz: ";
+	cin >> wyraz;
+
+
+	wyraz = odwroc(wyraz);
+	cout << "Odwrocony wyraz to: " << wyraz << endl;
+	return 0;
+
+}
