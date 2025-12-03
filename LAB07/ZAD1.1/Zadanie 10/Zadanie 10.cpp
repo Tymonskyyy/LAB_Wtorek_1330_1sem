e4e18b472f62a542bfd354b0db3b278d3e4307da
Krzyhu <lawecki.szymon24@gmail.com>
2025-12-03 15:04:09 +0100

    v.10 dokoncz

---
+++ LAB07/ZAD1.1/Zadanie 10/Zadanie 10.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int nwd(int a, int b) {
+	int j = 0;
+	int dzielniki[j]; // tablica na dzielniki
+	for (int i =1; i<=a || i <= b; i++) {
+		if (a % i == 0 && b % i == 0) {
+			return i;
+			for (j =0; j < ; j++) {
+				dzielniki[j] = i;
+		}
+	}
+}
+int main() {
+	cout << "Program do zliczania NWD dla podanych liczb." << endl;
+	int liczba = 0;
+	cout << "Podaj pierwsza liczbe: ";
+	cin >> liczba;
+	int liczba2 = 0;
+	cout << "Podaj druga liczbe: ";
+	cin >> liczba2;
+
+
+}
