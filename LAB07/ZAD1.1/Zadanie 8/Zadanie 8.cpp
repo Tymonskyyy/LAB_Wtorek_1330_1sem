ee8629b371ae61d653a420cc61a29d20df4ff406
Krzyhu <lawecki.szymon24@gmail.com>
2025-12-03 14:37:25 +0100

    V 1.8

---
+++ LAB07/ZAD1.1/Zadanie 8/Zadanie 8.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int silnia(int liczba_1) {
+	int wynik = 0;
+	for (int i = 1; i <= liczba_1; i++) {
+		if (i == 1) {
+			wynik = 1;
+		} else {
+			wynik = wynik * i;
+		}
+	}
+	return wynik;
+}
+int main() {
+	cout << "Program do liczenia silni liczby calkowitej dodatniej." << endl;
+	int liczba;
+	cout << "Podaj liczbe: ";
+	cin >> liczba;
+	silnia(liczba);
+	cout << "Silnia liczby " << liczba << " wynosi: " << silnia(liczba) << endl;
+	return 0;
+}
