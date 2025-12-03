925043b6c65c38d6333e6512d7952ea1b6d97658
Krzyhu <lawecki.szymon24@gmail.com>
2025-12-03 14:57:40 +0100

    V 7.9

---
+++ LAB07/ZAD1.1/ZAD 9/ZAD 9.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+int mnozenie_naturalnej(int liczba) {
+	int wynik = 0;
+	for (int i = 0; i < liczba; i++) {
+		wynik = wynik + i;
+
+	}
+	return wynik;
+}
+
+int main() {
+	int liczba = 0;
+	cout << "Podaj liczbe naturalna: ";
+	cin >> liczba;
+	if (liczba < 0) {
+		cout << "to nie jest liczba naturalna" << endl;
+	}
+	mnozenie_naturalnej(liczba);
+	cout << "Wynik mnozenia to: " << mnozenie_naturalnej(liczba) << endl;
+
+
+}
