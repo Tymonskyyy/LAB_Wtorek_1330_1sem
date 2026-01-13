4168be9e0f69c4c464e96bdcd1eb975376cd6b12
Krzyhu <lawecki.szymon24@gmail.com>
2026-01-13 14:00:01 +0100

    V1.1

---
+++ LAB08/Zadanie 1/Zadanie 1/Zadanie 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	int value = 123;
+	int* ptr;
+	ptr == &value;
+	cout << value<< endl;
+	cout << "Wartość zmiennej value: " << value << endl;
+	cin >> value;
+	cout << "Wprowadzono";
+	cout << endl;
+	cout << "Nowa wartość zmiennej value: " << endl;
+	cout << value << endl;
+}
