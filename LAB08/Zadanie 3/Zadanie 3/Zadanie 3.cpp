66c5074fc1c0326ce3551e0957c1f57fcd4d6a64
Krzyhu <lawecki.szymon24@gmail.com>
2026-01-13 14:32:42 +0100

    V1.3 Nie dokończne

--- LAB08/Zadanie 3/Zadanie 3/Zadanie 3.cpp
+++ LAB08/Zadanie 3/Zadanie 3/Zadanie 3.cpp
@@ -1,14 +1,1 @@
-﻿#include <iostream>
-using namespace std;
-int main() {
-	int tablica[10];
-	int* wsk;
-	wsk = tablica;
-	for (int i = 0; i < 10; i++) {
-		wsk[i] = 9;
-	}
-	//wyświetalnie tablicy
-	for (int z = 0; z < 10; z++) {
-		cout << tablica[z] << " ";
-	}
-}
+﻿
---
+++ LAB08/zadanie 3.1/zadanie 3.1/zadanie 3.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main() {
+	int rozmiar_tab = 0;
+	cout << "Podaj rozmiar tablicy: " << endl;
+	cin >> rozmiar_tab;
+	float tablica[rozmiar_tab];
+	tablica = new float tablica[rozmiar_tab];
+	for (int i = 0; i < rozmiar_tab; i++) {
+		tablica[i] = i;
+	}
+
+	for (int z = 0; z < rozmiar_tab; z++) {
+		cout << tablica[z] << " ";
+	}
+
+
+}
