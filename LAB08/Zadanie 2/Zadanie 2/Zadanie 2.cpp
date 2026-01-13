3a7fd2b4e831c6128da2c3aae6a10d9a6cde664f
Krzyhu <lawecki.szymon24@gmail.com>
2026-01-13 14:05:22 +0100

    V1.2

---
+++ LAB08/Zadanie 2/Zadanie 2/Zadanie 2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int jakastablica[10];
+	int* wsk;
+	wsk = jakastablica;
+	wsk = &jakastablica[0];
+	wsk++;
+	(*wsk)++;
+	
+	//wyświetlanie tablicy
+	for (int i = 0; i < 10; i++) {
+		cout << jakastablica[i] << " ";
+	}
+	
+	//zamiana rekordu 7 na 77
+
+	*(wsk + 5) = 77;
+	cout << "Wykonano zadanie polegające na zamianie. " << endl;
+
+	//wyświetlanie tablicy
+	for (int z = 0; z < 10; z++) {
+		cout << jakastablica[z] << " ";
+
+	}
+}
